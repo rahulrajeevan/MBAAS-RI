f88e0dd80e182b51262a30d2bab5d0fe17789bd6
rahulc <rahulc@RAHULC.user.nrifintech.com>
2014-02-28 14:20:26 +0530

    Css modified

--- App/src/view/login/login.js
+++ App/src/view/login/login.js
@@ -1,4 +1,27 @@
 angular.module('MBAAS-RI.view')
-	.controller('logInCtrl',function($scope){
-		
-	});
+	.controller('logInCtrl', ['$scope', '$state', '$http', '$ionicLoading', '$timeout',
+		function($scope, $state, $http, $ionicLoading, $timeout) {
+			$scope.signinuser = function(username, password) {
+				if (username && password) {
+					var names = [];
+					$http({
+						url: 'http://jsonstub.com/login',
+						method: 'POST',
+						headers: {
+							'JsonStub-User-Key': '7e801c34-1a24-4f2b-8ad0-07360aec6fb9',
+							'JsonStub-Project-Key': 'c6d07727-edb3-4858-a5f9-dad98ee03621'
+						}
+					})
+						.success(function(data, status, headers, config) {
+							console.log(data);
+						})
+						.error(function(data, status, headers, config) {
+							$scope.msg = 'Some error in connection !!!!';
+						});
+				}
+				else {
+					$scope.msg = 'Please Enter User Name and password';
+				}
+			};
+		}
+	]);
