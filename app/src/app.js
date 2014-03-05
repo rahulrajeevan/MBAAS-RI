d8245f589217493da1270b681c197a5c76755c83
rahulc <rahulc@RAHULC.user.nrifintech.com>
2014-03-05 09:52:38 +0530

    Mobileangularui files added

--- app/src/app.js
+++ app/src/app.js
@@ -1,21 +1,43 @@
-angular.module('fcdsaApp', ['ajoslin.mobile-navigate', 'shoppinpal.mobile-menu', 'fcdsaApp.directives', 'fcdsaApp.controllers', 'fcdsaApp.services'])
-	.config(function($routeProvider) {
-		$routeProvider.when("/settings", {
-			templateUrl: "src/view/settings.html"
-		}).when("/details:index", {
-			templateUrl: "src/view/details.html",
-			controller: "detailsCtrl",
-			transition: "modal" //this is overwritten by the go() in home.html
-		}).when("/popup", {
-			templateUrl: "src/view/popup.html",
-			transition: "modal"
-		}).when("/", {
-			templateUrl: "src/view/home.html",
-			controller: "homeCtrl"
-		}).otherwise({
-			redirectTo: "/"
-		});
+angular.module('MBAAS-RI', ["mobile-angular-ui"])
+	.config(function($routeProvider, $locationProvider) {
+		$routeProvider.when('/', {templateUrl: "../app/src/view/home/home.html"});
+		$routeProvider.otherwise('/');
 	})
+	.filter('range', function() {
+		return function(input, total) {
+			total = parseInt(total);
+			for (var i = 0; i < total; i++)
+				input.push(i);
+			return input;
+		};
+	})
+
+	.directive('exampleCode', function() {
+		return {
+			link: function(scope, elem, attrs) {
+				container = angular.element(document.getElementById(attrs.exampleCode));
+				html = container.html();
+				elem.empty().append("<pre><code>" + escapeHtml(html) + "</code></pre>")
+			}
+		};
+	})
+	.controller('MainController', function($rootScope, $scope) {
+
+	})
+
+var escapeHtml = function(str) {
+	var tagsToReplace = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;'
+	};
+	return str.replace(/[&<>]/g, function(tag) {
+		return tagsToReplace[tag] || tag;
+	});
+}
+
+
+
 
 
 
